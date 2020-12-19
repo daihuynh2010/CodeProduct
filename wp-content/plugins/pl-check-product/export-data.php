880fc8c18ef4adeab27ffc4547bfebdeee53889c
Quốc Đại <daihuynh2010@gmail.com>
2020-12-19 22:33:01 +0700

    daihq commit export excel

---
+++ wp-content/plugins/pl-check-product/export-data.php
@@ -0,0 +1,29 @@
+<?php
+require_once ('function.php');
+
+if(isset($_POST['export'])) { 
+    ExportData(); 
+} 
+function ExportData(){
+    $conn = Connection();
+    $sql = "Select * from pl_code_product";
+
+    $file_ending = "xls";
+    //header info for browser
+    header("Content-Type: application/xls");    
+    header("Content-Disposition: attachment; filename=ProductCode.xls");  
+    header("Pragma: no-cache"); 
+    header("Expires: 0");
+    $result = $conn->query($sql);
+
+    $isPrintHeader = false;
+    foreach ($result as $row) {
+            if (! $isPrintHeader) {
+                echo implode("\t", array_keys($row)) . "\n";
+                $isPrintHeader = true;
+            }
+            echo implode("\t", array_values($row)) . "\n";
+        }
+        exit();
+} 
+?>
--- wp-content/plugins/pl-check-product/views/create-code.php
+++ wp-content/plugins/pl-check-product/views/create-code.php
@@ -4,18 +4,24 @@ function PageCreateCode(){
 	<div class="wrap validate">
 		<h1>Create Code Product</h1>
 		<br/>
-		<button id="create">CreateTable</button>
-		<table class="form-table">
-			<tr style="width: 10%">
+		<!-- <button id="create">CreateTable</button> -->
+		<table >
+			<tr style="margin-bottom: 10px">
 				<th>
 					<label> Enter Product ID:</label>
 				</th>
-				<th>
-					<input type="text" id="ProductID"/>
-					<button id="genCode">Gen Code</button>
-				</th>
+				<td>
+					<input type="text" id="ProductID" style="margin-right: 10px"/>
+					<button id="genCode" class="button button-primary" style="margin-right: 10px">Creare Code</button>
+				</td>
+				<td>
+					<form action="<?php echo plugins_url("../export-data.php", __FILE__ )."" ?>" method="post">
+						<input class="button button-primary" type="submit" name="export" value="Export" />
+					</form>
+				</td>
 			</tr>
 			<tr>
+				<th></th>
 				<td>
 					<label id ="ProductCode"></label>
 				</td>
@@ -24,9 +30,13 @@ function PageCreateCode(){
 	</div>
 
 	<script type="text/javascript">
-		$("#create").click(function(){
+		// $("#create").click(function(){
+		// 	CreateTable();
+		// })
+		$(document).ready(function (){
 			CreateTable();
-		})
+		});
+
 		function CreateTable(){
 			$.ajax({
 				type: "GET",
@@ -47,10 +57,10 @@ function PageCreateCode(){
 		        url: "<?php echo plugins_url("../function.php", __FILE__ ).""  ?>",
 		        data:{ID:productID},
 		        success: function (output) {
-		           console.log(output);
 		           $("#ProductCode").html("Product Code: "+output);
 		        },
 		        error: function(xhr){
+		        	$("#ProductCode").html("Error: "+xhr.responseText);
 			        console.log('Request Status: ' + xhr.status + ' Status Text: ' + xhr.statusText + ' ' + xhr.responseText);
 			    }
 			});
