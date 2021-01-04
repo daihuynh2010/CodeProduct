12699a11362cf4608f10d5f3597d9df98015f8fe
Quốc Đại <daihuynh2010@gmail.com>
2021-01-04 17:21:11 +0700

    fixbug

---
+++ wp-content/plugins/pl-check-product/js/pl-custom.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	if($(".wpcf7-submit") != null){
+		var plCheckCodeLoading = 
+		"<div class='pl-check-product-modal'></div>";
+		$("body").append(plCheckCodeLoading);
+	}
+});
--- wp-content/plugins/pl-check-product/pl-check-product.php
+++ wp-content/plugins/pl-check-product/pl-check-product.php
@@ -32,35 +32,37 @@ function SetupMenu(){
 
 }
 
-
-add_action('admin_enqueue_scripts', 'wp_include_admin_js');
+add_action('admin_enqueue_scripts', 'wp_include_admin_js' );
 
 function wp_include_admin_js()
 {
 	wp_register_script( 'jquery-3.5.1.js', plugins_url("js/jquery-3.5.1.min.js",__FILE__) , array() );
 	wp_enqueue_script( 'jquery-3.5.1.js' );
-
-	wp_register_script( 'bootstrap.min.js', plugins_url("js/bootstrap.min.js",__FILE__) , array() );
-	wp_enqueue_script( 'bootstrap.min.js' );
-
-	wp_register_style('bootstrap.min.css', plugins_url("css/bootstrap.min.css",__FILE__) , array() );
-	wp_enqueue_style('bootstrap.min.css');
 }
 
+add_action('wp_enqueue_scripts', 'wp_include_pl_check_code', 10, 0 );
+
+function wp_include_pl_check_code(){
+	wp_register_script( 'jquery-3.5.1.js', plugins_url("js/jquery-3.5.1.min.js",__FILE__) , array() );
+	wp_enqueue_script( 'jquery-3.5.1.js' );
+	wp_register_script( 'pl-check-code-custom.js', plugins_url("js/pl-custom.js",__FILE__) , array() );
+	wp_enqueue_script( 'pl-check-code-custom.js' );
+
+	wp_register_style( 'pl-check-code-custom.css', plugins_url("css/pl-custom.css",__FILE__) , array() );
+	wp_enqueue_style( 'pl-check-code-custom.css' );
+}
 
 add_action( 'wp_footer', 'GetCodeData', 100 );
 
 function GetCodeData() { 
 ?>
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script>
     (function () {
 
         $ = jQuery.noConflict();
 
         $(".wpcf7-submit").on('click',function(){
-			waitingDialog.show();
+        	$("body").addClass("pl-check-product-loading");
         	var code = $(this).parent().parent().find('input[name ="code"]').val();
             var name = $(this).parent().parent().find('input[name ="hotenkh"]').val();
             var phoneNumber = $(this).parent().parent().find('input[name ="sdtkh"]').val();
@@ -69,9 +71,9 @@ function GetCodeData() {
 
             var codeinput = $(this).parent().parent().find('input[name ="code"]');
         	$(".wpcf7-submit").prop("disabled",true);
+		    $(".wpcf7-not-valid-tip").hide();
             if(code != null && code != "" && name != null && name != ""
-            	&& phoneNumber != null && phoneNumber != "" && email != null && email != ""
-            	&& diachi != null && diachi != ""){
+            	&& phoneNumber != null && phoneNumber != "" && diachi != null && diachi != ""){
 	            
 	            $.ajax({
 	                method : 'POST',
@@ -81,106 +83,73 @@ function GetCodeData() {
 	                },
 	                success : function( response ) {
 	                    console.log( response );
-	                    if(response == "0")
+	                    switch(response) 
 	                    {
-        					$(".wpcf7-submit").submit();
-	                    }
-	                    else
-	                    {
-	                		waitingDialog.hide();
-	                    	// $("#checkfail").show();
-	                    	$(".wpcf7-not-valid-tip").hide();
-		                    if( $("#resultCheckCode").html() == null || $("#resultCheckCode").html() == ""){
-		                   		codeinput.parent().parent().append("<span class='wpcf7-not-valid-tip' id='resultCheckCode'>Mã sản phẩm không đúng </span> <input hidden type='text' name='coderesult' value='"+response + "'/> <a href='#checkfail'>click</a>");
-		               		}	
-		               		else
-		               		{
-		               			$("#resultCheckCode").val(response);
-		               		}
+	                    	case "0":
+        						$(".wpcf7-submit").submit();
+        						break;
 
-		                   	$('#checkfail').trigger('click');
+	                    	case "1":
+	                    		$("body").removeClass("pl-check-product-loading");
+		                    	$("#resultCheckCode").show();
+			                    if( $("#resultCheckCode").html() == null || $("#resultCheckCode").html() == ""){
+			                   		codeinput.parent().parent().append("<span class='wpcf7-not-valid-tip' id='resultCheckCode'>Mã sản phẩm không đúng</span> <input hidden type='text' name='coderesult' value='"+response + "'/> <a href='#checkfail' style ='display:none'>click</a>");
+			               		}	
+			               		else
+			               		{
+			               			$("#resultCheckCode").html("Mã sản phẩm không đúng");
+			               		}
+
+			                   	$('#checkfail').trigger('click');
+			                   	break;
+			                case "2":
+		                		
+	                    		$("body").removeClass("pl-check-product-loading");
+		                    	$("#resultCheckCode").show();
+			                    if( $("#resultCheckCode").html() == null || $("#resultCheckCode").html() == ""){
+			                   		codeinput.parent().parent().append("<span class='wpcf7-not-valid-tip' id='resultCheckCode'>Sản phẩm đã được xác minh</span> <input hidden type='text' name='coderesult' value='"+response + "'/> <a href='#checkfail' style ='display:none'>click</a>");
+			               		}	
+			               		else
+			               		{
+			               			$("#resultCheckCode").html("Sản phẩm đã được xác minh");
+			               		}
+
+			                   	$('#checkfail').trigger('click');
+			                   	break;
+			                case "3":
+
+	                    		$("body").removeClass("pl-check-product-loading");
+		                    	$("#resultCheckCode").show();
+			                    if( $("#resultCheckCode").html() == null || $("#resultCheckCode").html() == ""){
+			                   		codeinput.parent().parent().append("<span class='wpcf7-not-valid-tip' id='resultCheckCode'>Xảy ra lỗi trong quá trình kiểm tra, vui lòng thử lại</span> <input hidden type='text' name='coderesult' value='"+response + "'/>");
+			               		}	
+			               		else
+			               		{
+			               			$("#resultCheckCode").html("Xảy ra lỗi trong quá trình kiểm tra, vui lòng thử lại");
+			               		}
+			                	break;
 	                    }
         				$(".wpcf7-submit").prop("disabled",false);
 	                },
 	                error: function(xhr){
-	                	waitingDialog.hide();
+	                    $("body").removeClass("pl-check-product-loading");
 				        console.log('Request Status: ' + xhr.status + ' Status Text: ' + xhr.statusText + ' ' + xhr.responseText);
 				    }
 	            });
 	    	}
 	    	else{
-	    		waitingDialog.hide();
+	            $("body").removeClass("pl-check-product-loading");
 	    		$(".wpcf7-submit").submit();
 	    		$(".wpcf7-submit").prop("disabled",false);
 	    	}
         });
 
         document.addEventListener( 'wpcf7mailsent', function( event ) {
-		  	waitingDialog.hide();
+	        $("body").removeClass("pl-check-product-loading");
 		}, false );
 
 
-        var waitingDialog = waitingDialog || (function ($) {'use strict';
-
-		// Creating modal dialog's DOM
-		var $dialog = $(
-			'<div class="modal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-hidden="true" style="padding-top:15%; overflow-y:visible;">' +
-			'<div class="modal-dialog modal-m">' +
-			'<div class="modal-content">' +
-				'<div class="modal-header"><h3 style="margin:0;"></h3></div>' +
-				'<div class="modal-body">' +
-					'<div class="progress progress-striped active" style="margin-bottom:0;"><div class="progress-bar" style="width: 100%"></div></div>' +
-				'</div>' +
-			'</div></div></div>');
-
-		return {
-			/**
-			 * Opens our dialog
-			 * @param message Custom message
-			 * @param options Custom options:
-			 * 				  options.dialogSize - bootstrap postfix for dialog size, e.g. "sm", "m";
-			 * 				  options.progressType - bootstrap postfix for progress bar type, e.g. "success", "warning".
-			 */
-			show: function (message, options) {
-				// Assigning defaults
-				if (typeof options === 'undefined') {
-					options = {};
-				}
-				if (typeof message === 'undefined') {
-					message = 'Đang kiểm tra';
-				}
-				var settings = $.extend({
-					dialogSize: 'm',
-					progressType: '',
-					onHide: null // This callback runs after the dialog was hidden
-				}, options);
-
-				// Configuring dialog
-				$dialog.find('.modal-dialog').attr('class', 'modal-dialog').addClass('modal-' + settings.dialogSize);
-				$dialog.find('.progress-bar').attr('class', 'progress-bar');
-				if (settings.progressType) {
-					$dialog.find('.progress-bar').addClass('progress-bar-' + settings.progressType);
-				}
-				$dialog.find('h3').text(message);
-				// Adding callbacks
-				if (typeof settings.onHide === 'function') {
-					$dialog.off('hidden.bs.modal').on('hidden.bs.modal', function (e) {
-						settings.onHide.call($dialog);
-					});
-				}
-				// Opening dialog
-				$dialog.modal();
-			},
-			/**
-			 * Closes dialog
-			 */
-			hide: function () {
-				$dialog.modal('hide');
-			}
-		};
-
-		})(jQuery);
-	    })();
+	})();
 </script>
 
 <?php
